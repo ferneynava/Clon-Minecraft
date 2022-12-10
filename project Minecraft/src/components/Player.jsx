8c06b6ed0e8218faf358efd8927fade65bf81f3c
ferneynava <ferneynava@gmail.com>
2022-12-09 19:00:53 -0500

    :white_check_mark: Add: character movement
    Add: character movement

--- project Minecraft/src/components/Player.jsx
+++ project Minecraft/src/components/Player.jsx
@@ -2,8 +2,20 @@ import { useSphere } from '@react-three/cannon'
 import { useFrame, useThree } from '@react-three/fiber'
 import { useEffect, useRef } from 'react'
 import { Vector3 } from 'three'
+import { useKeyboard } from '../hooks/useKeyboard.js'
+
+const CHARACTER_SPEED = 5
+const CHARACTER_JUMP_FORCE = 10
 
 export const Player = () => {
+  const {
+    moveBackward,
+    moveForward,
+    moveLeft,
+    moveRight,
+    jump
+  } = useKeyboard()
+
   const { camera } = useThree()
   const [ref, api] = useSphere(() => ({
     mass: 1,
@@ -35,9 +47,31 @@ export const Player = () => {
         pos.current[2] // z
       )
     )
-    // Moviemdo hacia adelante
-    // Pero esto tendría que ser con el teclado
-    api.velocity.set(0, 0, -1)
+
+    const direction = new Vector3()
+
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    )
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    )
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(CHARACTER_SPEED)
+      .applyEuler(camera.rotation)
+
+    api.velocity.set(
+      direction.x,
+      vel.current[1],
+      direction.z
+    )
   })
 
   return (
